6b08b49a32003c526c0ca17058ed9c95b6a1452c
Rhaíssa Lima <rhaissasbgl@gmail.com>
2025-08-07 14:53:47 -0400

    added file reader

--- io/github/rhaissabg/shelterpets/ShelterApp.java
+++ io/github/rhaissabg/shelterpets/ShelterApp.java
@@ -1,9 +1,39 @@
 package io.github.rhaissabg.shelterpets;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class ShelterApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		File file = new File("src/resources/shelter_questions.txt");
+		Scanner sc = new Scanner(System.in);
+		try {
+			Scanner reader = new Scanner(file);
+			System.out.print(reader.nextLine() + " ");
+			String name = sc.nextLine();
+			System.out.print(reader.nextLine() + " ");
+			String type = sc.next();
+			System.out.print(reader.nextLine() + " ");
+			String sex = sc.next();
+			System.out.print(reader.nextLine() + " ");
+			sc.nextLine();
+			String adress = sc.nextLine();
+			System.out.print(reader.nextLine() + " ");
+			int age = sc.nextInt();
+			System.out.print(reader.nextLine() + " ");
+			double weight = sc.nextDouble();
+			System.out.print(reader.nextLine() + " ");
+			sc.nextLine();
+			String breed = sc.nextLine();
+			reader.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		sc.close();
 
 	}
 
