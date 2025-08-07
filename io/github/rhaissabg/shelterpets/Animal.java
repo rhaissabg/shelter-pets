c64aa87c845ad0051eff6a968e021287b2dc3994
Rhaíssa Lima <rhaissasbgl@gmail.com>
2025-08-07 14:33:36 -0400

    first commit

---
+++ io/github/rhaissabg/shelterpets/Animal.java
@@ -0,0 +1,5 @@
+package io.github.rhaissabg.shelterpets;
+
+public class Animal {
+
+}
---
+++ io/github/rhaissabg/shelterpets/ShelterApp.java
@@ -0,0 +1,10 @@
+package io.github.rhaissabg.shelterpets;
+
+public class ShelterApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
