900e3fb1c09a8f07f046ac3304c75f725b689853
Rhaíssa Lima <rhaissasbgl@gmail.com>
2025-08-10 14:40:16 -0400

    first question validation

--- io/github/rhaissabg/shelterpets/ShelterApp.java
+++ io/github/rhaissabg/shelterpets/ShelterApp.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class ShelterApp {
 
 	public static void main(String[] args) {
-		
+
 		File file = new File("src/resources/shelter_questions.txt");
 		Scanner sc = new Scanner(System.in);
 		int option = -1;
@@ -22,6 +22,7 @@ public class ShelterApp {
 				System.out.println("5 - List pets by some criteria (age, name, breed)");
 				System.out.println("6 - Exit");
 				option = sc.nextInt();
+				sc.nextLine();
 				if (!(option >= 1 && option <= 6)) {
 					System.out.println("\nPlease, enter a valid number");
 				}
@@ -33,26 +34,32 @@ public class ShelterApp {
 			case 1:
 				try {
 					Scanner reader = new Scanner(file);
-					System.out.print(reader.nextLine() + " ");
-					sc.nextLine();
-					String name = sc.nextLine();
-					System.out.print(reader.nextLine() + " ");
-					String type = sc.next();
-					System.out.print(reader.nextLine() + " ");
-					String sex = sc.next();
-					System.out.print(reader.nextLine() + " ");
-					sc.nextLine();
-					String adress = sc.nextLine();
-					System.out.print(reader.nextLine() + " ");
-					int age = sc.nextInt();
-					System.out.print(reader.nextLine() + " ");
-					double weight = sc.nextDouble();
-					System.out.print(reader.nextLine() + " ");
-					sc.nextLine();
-					String breed = sc.nextLine();
+					String question = reader.nextLine();
+					boolean validName = false;
+					while (!validName) {
+						System.out.print(question + " ");
+						String name = sc.nextLine();
+						String[] nameParts = name.trim().split("\\s+");
+						boolean hasNumberOrSpecialCharacter = false;
+						for (String part : nameParts) {
+							if (!part.matches("^[a-zA-Z]+$")) {
+								hasNumberOrSpecialCharacter = true;
+								break;
+							}
+						}
+						try {
+							if (nameParts.length > 1 && !hasNumberOrSpecialCharacter) {
+								validName = true;
+							} else {
+								throw new IllegalArgumentException();
+							}
+						} catch (IllegalArgumentException e) {
+							System.out.println("\nPlease, enter first and last name.\nUse with letters only, no numbers or special characters.");
+							e.printStackTrace();
+							System.out.println();
+						}
+					}
 					reader.close();
-					Animal animal = new Animal(name, type, sex, adress, age, weight, breed);
-					System.out.println(animal);
 				} catch (FileNotFoundException e) {
 					e.printStackTrace();
 				}
@@ -69,9 +76,53 @@ public class ShelterApp {
 				break;
 			}
 		}
-		
+
 		sc.close();
 
 	}
 
 }
+
+//				try {
+//					Scanner reader = new Scanner(file);
+//					boolean validName = false;
+//					String name = null;
+//					String question = reader.nextLine() + " ";
+//					while (!validName) {
+//						System.out.print(question);
+//						name = sc.nextLine();
+//						String[] nameParts = name.trim().split("\\s+");
+//						boolean hasNumberOrSpecialCharacter = false;
+//						for (int i = 0; i < nameParts.length; i++) {
+//							if (!nameParts[i].matches("^[a-zA-Z\\\\s]*$")) {
+//								hasNumberOrSpecialCharacter = true;
+//								break;
+//							}
+//						}
+//						if (nameParts.length < 2 || hasNumberOrSpecialCharacter) {
+////							System.out.println("Please, enter first and last name and only letters!");
+//							throw new IllegalArgumentException("Please, enter first and last name and only letters!");
+//						} else {
+//							validName = true;
+//						}
+//					}
+//					System.out.print(reader.nextLine() + " ");
+//					String type = sc.next();
+//					System.out.print(reader.nextLine() + " ");
+//					String sex = sc.next();
+//					System.out.print(reader.nextLine() + " ");
+//					sc.nextLine();
+//					String adress = sc.nextLine();
+//					System.out.print(reader.nextLine() + " ");
+//					int age = sc.nextInt();
+//					System.out.print(reader.nextLine() + " ");
+//					double weight = sc.nextDouble();
+//					System.out.print(reader.nextLine() + " ");
+//					sc.nextLine();
+//					String breed = sc.nextLine();
+//					reader.close();
+//					Animal animal = new Animal(name, type, sex, adress, age, weight, breed);
+//					System.out.println(animal);
+//				} catch (FileNotFoundException e) {
+//					e.printStackTrace();
+//				}
